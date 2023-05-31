afa106095f23a139e35be599dcbd63de8fccd423
Sarah <sarahvictoria230@gmail.com>
2023-05-31 10:23:40 -0300

    Creating edit functinality and fixing the check functionality

--- js/index.js
+++ js/index.js
@@ -43,19 +43,47 @@ function createTask() {
     button.addEventListener('click', checkTask);
   })
 
-
-
   deleteButton.forEach(button => {
     button.addEventListener('click', deleteTask);
   });
+
+  editButton.forEach(button => {
+    button.addEventListener('click', editTask);
+  })
 }
 
 function checkTask() {
-  const taskContent = document.querySelector('.task-content');
+  const taskContent = this.parentNode.previousElementSibling;
 
-  taskContent.style.textDecoration = 'line-through';
+  taskContent.style.textDecoration === 'line-through' ? taskContent.style.textDecoration = 'none' : taskContent.style.textDecoration = 'line-through';
 }
 
 function deleteTask() {
   this.parentNode.parentNode.remove()
+}
+
+function editTask() {
+  const task = this.closest('.task');
+  const taskContent = document.querySelector('.task-content');
+  const editButton = task.querySelector('.btnEdit');
+
+  taskContent.setAttribute('contenteditable', true);
+  taskContent.focus();
+
+  taskContent.addEventListener('keydown', handleKeyDown);
+
+  function handleKeyDown(event) {
+    if (event.key === 'Enter') {
+      event.preventDefault();
+      taskContent.blur();
+    }
+  }
+
+  taskContent.addEventListener('blur', saveEditedTask);
+
+  function saveEditedTask() {
+    taskContent.setAttribute('contenteditable', false);
+    taskContent.removeEventListener('keydown', handleKeyDown);
+    taskContent.removeEventListener('blur', saveEditedTask);
+  }
 }
